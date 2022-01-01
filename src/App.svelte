8845668b1afc7e29c2c91b10a95f60053006d824
FilippoBertozzi <47744703+FilippoBertozzi@users.noreply.github.com>
2022-01-01 19:45:21 +0100

    Created login form + some design
    Creato il form del login necessario da parte del manager e stylato un po'

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,50 @@
 <script>
-	let name = 'world';
+	
 </script>
+<body>
+	<h1>NoloNolo</h1>
+	<h2>Manager login</h2>
+	<div>
+		<p>Username:</p><input type="text">
+		<p>Password:</p><input type="text">
+		<button onclick="">Login</button>
+	</div>
+</body>
 
-<h1>Hello {name}!</h1>
+<style>
+	body{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	h1{
+		font-size: 4rem;
+		text-align: center;
+		color: #5AEAFB;
+	}
+	h2{
+		font-size: 3rem;
+		text-align: center;
+		color: #5AEAFB;
+	}
+	div{
+		width: 50%;
+		display: flex;
+		flex-direction: column;
+  		border: 3px solid #b7e0f4;
+		  border-radius: 2rem;
+		padding: 1em;
+	}
+	p{
+		font-weight: bold;
+		text-align: center;
+	}
+	button{
+		font-weight: bold;
+		color: white;
+		background-color:#5AEAFB; /*per il logout colore-> #C85662*/
+		border:none;
+		border-radius: 3px;
+	}
+
+</style>
