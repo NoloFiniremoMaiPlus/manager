bc9c8e2a6d1adcd15761cb904c5d43afd6aa3930
FilippoBertozzi <47744703+FilippoBertozzi@users.noreply.github.com>
2022-01-20 18:00:41 +0100

    Rentals by Admin done
    Quasi finito anche Revenue by Admin

--- src/statistiche.svelte
+++ src/statistiche.svelte
@@ -8,8 +8,8 @@
     import Chart from 'chart.js/auto';
 
 	const foucus = useFocus(); //accessibility
-    
-    async function RevenueByAdmin(){
+
+    async function getUsers(){
         const res = await fetch(api+'/users', { 
 			method: 'GET',
 			headers: {
@@ -19,25 +19,76 @@
 			},
 		})
         let json = await res.json()
-        let print = json.results.filter((user) => user.role != "user")
-        console.log(json)
-        console.log(print)
-        let usernames = [];
-        for(let i=0;i<print.length;i++){
-            usernames.push(print[i].username)
+        return json
+    }
+
+    async function getRentals(){ //userid = 61e59a87827a2a7757adb6e7
+        const res = await fetch(api+'/rentals', { 
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+      			'Content-Type': 'application/json',
+                'Authorization': "Bearer " + localStorage.getItem("accessToken"),
+			},
+		})
+        let json = await res.json() 
+        return json.results
+    }
+
+    async function RevenueByAdmin(){
+        let rentals = await getRentals()
+        console.log(rentals)
+        
+        let users = await getUsers()
+        console.log(users)
+        let admins = users.results.filter((user) => user.role != "user")
+
+        let values = []
+        for(let i=0;i<admins.length;i++){
+            values[i]=0
         }
+        let usernames = []
+
+        for(let i=0;i<admins.length;i++){
+            usernames.push(admins[i].username)
+            for(let j=0;j<rentals.length;j++){
+                if(admins[i].username==rentals[j].resp.username){
+                    values[i]++
+                }
+            }
+        }
+        console.log(values)
         console.log(usernames)
         console.log(typeof usernames)
         //return usernames;
-        const ctx1 = document.getElementById('adminRevenue').getContext('2d');
-        const ctx2 = document.getElementById('adminRentals').getContext('2d');
-        const adminsRevenue = new Chart(ctx1, {
+
+        const ctx1 = document.getElementById('adminRentals').getContext('2d');
+        const ctx2 = document.getElementById('adminRevenue').getContext('2d');
+       
+        const adminsRentals = new Chart(ctx1, {
             type: 'bar',
             data:{
                 labels: usernames,
                 datasets:[{
-                    label: 'Revenue by Admin',
-                    data:[10,5,3,1]
+                    label: 'Rentals by Admin',
+                    data: values,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
                 }]
             },
             options:{
@@ -60,14 +111,14 @@
     }
 </script>
 <div class="divButtons" use:foucus>
-    <button class="selettori" >doChart</button> <!--Clienti-->
-    <button class="selettori" on:click={RevenueByAdmin}>getAdmins</button> <!--Dipendenti-->
+    <button class="selettori" on:click={getRentals}>getRentals</button> <!--Clienti-->
+    <button class="selettori" on:click={RevenueByAdmin}>RevenueByAdmin</button> <!--Dipendenti-->
     <button class="selettori">Prodotti</button>
     <button class="selettori">Noleggi</button>
 </div>
 <div class="grafici">
-    <canvas id="adminRevenue" width="400" height="400"></canvas>
     <canvas id="adminRentals" width="400" height="400"></canvas>
+    <canvas id="adminRevenue" width="400" height="400"></canvas>
 </div>
 <style>
     .divButtons{
